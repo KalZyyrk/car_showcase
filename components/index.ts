ad46a534a987ef16cd1dea67beee4ac3a6981be0
Ulysse <u.hischke@outlook.fr>
2023-06-11 12:06:56 +0200

    add search bar

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,8 @@ import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from './SearchManufacturer';
 
-export { Hero, CustomButton, Footer, Navbar };
+export { Hero, CustomButton, Footer, Navbar, CustomFilter, SearchBar, SearchManufacturer };
