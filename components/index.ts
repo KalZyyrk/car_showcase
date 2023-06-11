7dddfa12ef046524d610938b5e63ed303ada393f
Ulysse <u.hischke@outlook.fr>
2023-06-11 13:02:09 +0200

    add car card

--- components/index.ts
+++ components/index.ts
@@ -4,6 +4,18 @@ import Hero from "./Hero";
 import Navbar from "./Navbar";
 import CustomFilter from "./CustomFilter";
 import SearchBar from "./SearchBar";
-import SearchManufacturer from './SearchManufacturer';
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
-export { Hero, CustomButton, Footer, Navbar, CustomFilter, SearchBar, SearchManufacturer };
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  Navbar,
+  CustomFilter,
+  SearchBar,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+};
