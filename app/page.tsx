a5e8b38f18c75a1fa99fe79e5a57b15b353a485c
Ulysse <u.hischke@outlook.fr>
2023-06-14 11:05:29 +0200

    add HomeProps

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
+import { FilterProps } from "@/types";
 import { fetchCars } from "@/utils";
 
-export default async function Home({ searchParams }) {
+interface HomeProps {
+  searchParams: FilterProps;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
